8ecdd640ff07a379f420610bd8aa7017a794e5ae
Maksim Golubev <69137770+devgomax@users.noreply.github.com>
2025-02-16 17:39:41 +0600

    Hw03 frequency analysis (#3)
    * HW3 is completed
    
    * linter fix
    
    ---------
    
    Co-authored-by: Golubev Maksim Vladislavovich <maksgolubev@ozon.ru>

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,32 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"cmp"
+	"slices"
+	"strings"
+)
+
+// Top10 возвращает до 10 наиболее часто встречаемых в строке слов.
+func Top10(raw string) []string {
+	counter := make(map[string]int)
+
+	for _, w := range strings.Fields(raw) {
+		counter[w]++
+	}
+
+	words := make([]string, 0, len(counter))
+	for k := range counter {
+		words = append(words, k)
+	}
+
+	slices.SortFunc(words, func(a, b string) int {
+		return cmp.Or(
+			cmp.Compare(counter[b], counter[a]),
+			strings.Compare(a, b),
+		)
+	})
+
+	topCount := min(10, len(words))
+
+	return words[:topCount]
 }
