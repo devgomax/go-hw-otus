928bc06650e892999617a5e3c0aaef5bde0a4b8f
Maksim Golubev <69137770+devgomax@users.noreply.github.com>
2025-03-19 11:08:50 +0600

    Hw06 pipeline execution (#6)
    * HW06 is completed
    
    * HW06 is completed
    
    * HW06 is completed
    
    * HW06 is completed
    
    ---------
    
    Co-authored-by: Golubev Maksim Vladislavovich <maksgolubev@ozon.ru>

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,14 +1,58 @@
 package hw06pipelineexecution
 
+// Channel type aliases.
 type (
 	In  = <-chan interface{}
 	Out = In
 	Bi  = chan interface{}
 )
 
+// Stage function that starts inner goroutine to do some work with data and returns channel.
 type Stage func(in In) (out Out)
 
+// ExecutePipeline executes a pipeline built from the provided stages.
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+
+	if in == nil { // устраняет deadlock в кейсе, когда вместо входного канала передан nil
+		rChan := make(Bi)
+		defer close(rChan)
+		out = rChan
+	}
+
+	for _, stage := range stages {
+		out = stage(cancellableChan(out, done))
+	}
+
+	return cancellableChan(out, done)
+}
+
+func cancellableChan(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer func() {
+			close(out)
+			<-in
+		}()
+
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+
+				select {
+				case <-done:
+					return
+				case out <- v:
+				}
+			}
+		}
+	}()
+
+	return out
 }
