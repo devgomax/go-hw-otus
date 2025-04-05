55d34a405301a5751d85c4659983ddd179962328
Golubev Maksim Vladislavovich <maksgolubev@ozon.ru>
2025-04-05 19:20:59 +0600

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3" //nolint:depguard
 )
 
 var (
@@ -9,7 +13,52 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+// Copy копирует limit байт из файла по пути fromPath в файл по пути toPath с отступом offset.
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	if fInfo.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 || offset+limit > fInfo.Size() {
+		limit = fInfo.Size() - offset
+	}
+
+	fileFrom, err := os.OpenFile(fromPath, os.O_RDONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer fileFrom.Close()
+
+	if offset != 0 {
+		_, err = fileFrom.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(fileFrom)
+
+	fileTo, err := os.OpenFile(toPath, os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		return err
+	}
+
+	defer fileTo.Close()
+
+	_, err = io.CopyN(fileTo, barReader, limit)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,107 @@
 package main
 
-import "testing"
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const testdata = "testdata/"
+
+func getMD5Sum(f *os.File) (string, error) {
+	sum := md5.New()
+	if _, err := io.Copy(sum, f); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%X", sum.Sum(nil)), nil
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []struct {
+		refTo  string
+		offset int64
+		limit  int64
+	}{
+		{refTo: testdata + "out_offset0_limit0.txt", offset: 0, limit: 0},
+		{refTo: testdata + "out_offset0_limit10.txt", offset: 0, limit: 10},
+		{refTo: testdata + "out_offset0_limit1000.txt", offset: 0, limit: 1000},
+		{refTo: testdata + "out_offset0_limit10000.txt", offset: 0, limit: 10000},
+		{refTo: testdata + "out_offset100_limit1000.txt", offset: 100, limit: 1000},
+		{refTo: testdata + "out_offset6000_limit1000.txt", offset: 6000, limit: 1000},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.refTo, func(t *testing.T) {
+			tempFile, err := os.CreateTemp("", "test_result")
+			require.NoError(t, err)
+			defer os.Remove(tempFile.Name())
+
+			err = Copy(testdata+"input.txt", tempFile.Name(), tt.offset, tt.limit)
+			require.NoError(t, err)
+
+			refFile, err := os.OpenFile(tt.refTo, os.O_RDONLY, 0644)
+			require.NoError(t, err)
+			defer refFile.Close()
+
+			refHashSum, err := getMD5Sum(refFile)
+			require.NoError(t, err)
+
+			givenHashSum, err := getMD5Sum(tempFile)
+			require.NoError(t, err)
+
+			require.Equal(t, refHashSum, givenHashSum)
+		})
+	}
+}
+
+func TestCopyWithOversizeOffset(t *testing.T) {
+	t.Run("offset_gt_len_file", func(t *testing.T) {
+		tempFile, err := os.CreateTemp("", "test_result")
+		require.NoError(t, err)
+		defer os.Remove(tempFile.Name())
+
+		fInfo, err := os.Stat(testdata + "input.txt")
+		require.NoError(t, err)
+
+		err = Copy(testdata+"input.txt", tempFile.Name(), fInfo.Size()+1, 0)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
+}
+
+func TestCopyDevNull(t *testing.T) {
+	t.Run("unknown_len_file", func(t *testing.T) {
+		tempFile, err := os.CreateTemp("", "test_result")
+		require.NoError(t, err)
+		defer os.Remove(tempFile.Name())
+
+		err = Copy("/dev/null", tempFile.Name(), 1, 1)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+}
+
+func TestCopyDevUrandom(t *testing.T) {
+	t.Run("unknown_len_file", func(t *testing.T) {
+		tempFile, err := os.CreateTemp("", "test_result")
+		require.NoError(t, err)
+		defer os.Remove(tempFile.Name())
+
+		err = Copy("/dev/urandom", tempFile.Name(), 1, 1)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+}
+
+func TestCopyFromInvalidPath(t *testing.T) {
+	t.Run("copy_from_invalid_path", func(t *testing.T) {
+		tempFile, err := os.CreateTemp("", "test_result")
+		require.NoError(t, err)
+		defer os.Remove(tempFile.Name())
+
+		err = Copy(testdata+"dmalsdkasdkasfdmd.txt", tempFile.Name(), 1, 1)
+		require.Error(t, err)
+	})
 }
