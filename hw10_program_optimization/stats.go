00489571fa6711d48c7078b3f7526207e874e7bd
Golubev Maksim Vladislavovich <maksgolubev@ozon.ru>
2025-04-24 02:05:18 +0600

    HW10 is completed

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -3,7 +3,6 @@ package hw10programoptimization
 import (
 	"bufio"
 	"errors"
-	"fmt"
 	"io"
 	"strings"
 
@@ -22,43 +21,21 @@ type DomainStat map[string]int
 
 // GetDomainStat calculates domain statistics from input reader.
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-func getUsers(r io.Reader) ([]User, error) {
 	var (
-		user  User
-		users []User
+		result  = make(DomainStat)
+		err     error
+		user    *User
+		json    = jsoniter.ConfigCompatibleWithStandardLibrary
+		scanner = bufio.NewScanner(r)
+		suffix  = "." + domain
 	)
 
-	json := jsoniter.ConfigCompatibleWithStandardLibrary
-
-	scanner := bufio.NewScanner(r)
-
 	for i := 0; scanner.Scan(); i++ {
-		if err := json.Unmarshal(scanner.Bytes(), &user); err != nil {
+		if err = json.Unmarshal(scanner.Bytes(), &user); err != nil {
 			return nil, errors.Join(err, ErrInvalidJSON)
 		}
 
-		users = append(users, user)
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-
-	return users, nil
-}
-
-func countDomains(u []User, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
-		if strings.HasSuffix(user.Email, "."+domain) {
+		if strings.HasSuffix(user.Email, suffix) {
 			idx := strings.LastIndex(user.Email, "@")
 			if idx == -1 {
 				continue
@@ -66,6 +43,10 @@ func countDomains(u []User, domain string) (DomainStat, error) {
 
 			result[strings.ToLower(user.Email[idx+1:])]++
 		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		return nil, err
 	}
 
 	return result, nil
