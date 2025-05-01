d071cb583c183ebdb9280f825b7075feb529b033
Maksim Golubev <69137770+devgomax@users.noreply.github.com>
2025-05-01 16:32:47 +0600

    Hw10 program optimization (#10)
    * HW10 is completed
    
    * HW10 is completed
    
    * HW10 is completed
    
    * HW10 is completed
    
    ---------
    
    Co-authored-by: Golubev Maksim Vladislavovich <maksgolubev@ozon.ru>

--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -1,3 +1,4 @@
+//go:build !bench
 // +build !bench
 
 package hw10programoptimization
@@ -36,4 +37,38 @@ func TestGetDomainStat(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, DomainStat{}, result)
 	})
+}
+
+func TestGetDomainStatInvalidData(t *testing.T) {
+	t.Run("invalid email skipped", func(t *testing.T) {
+		data := `{"Email":"aliquid_qui_ea-Browsedrive.gov"}`
+		result, err := GetDomainStat(bytes.NewBufferString(data), "gov")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{}, result)
+	})
+
+	t.Run("invalid json errors", func(t *testing.T) {
+		data := `["Id":1,"Name":"N","Username":"U","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"1","Password":"1","Address":"1"]`
+		_, err := GetDomainStat(bytes.NewBufferString(data), "gov")
+		require.ErrorIs(t, err, ErrInvalidJSON)
+	})
+
+	t.Run("line without email skipped", func(t *testing.T) {
+		data := `{"Gmail":"aliquid_qui_ea@Browsedrive.gov"}`
+		result, err := GetDomainStat(bytes.NewBufferString(data), "gov")
+		require.NoError(t, err)
+		require.Equal(t, DomainStat{}, result)
+	})
+}
+
+func BenchmarkGetDomainStat(b *testing.B) {
+	data := `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}
+{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}
+{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}
+{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
+{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
+
+	for i := 0; i < b.N; i++ {
+		GetDomainStat(bytes.NewBufferString(data), "com")
+	}
 }
