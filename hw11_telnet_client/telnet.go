4cd56acfb49fa9c2060187c10e8ba68985bb73f4
Maksim Golubev <69137770+devgomax@users.noreply.github.com>
2025-05-02 17:12:03 +0600

    HW11 is completed (#11)
    * HW11 is completed
    
    * HW11 is completed
    
    * HW11 is completed
    
    ---------
    
    Co-authored-by: Golubev Maksim Vladislavovich <maksgolubev@ozon.ru>

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +14,68 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	conn    net.Conn
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (c *telnetClient) Connect() error {
+	var err error
+
+	c.conn, err = net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return fmt.Errorf("failed to open connection: %w", err)
+	}
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *telnetClient) Close() error {
+	c.in.Close()
+	if c.conn != nil {
+		return c.conn.Close()
+	}
+	return nil
+}
+
+func (c *telnetClient) Send() error {
+	buf := make([]byte, 1024)
+
+	n, err := c.in.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	if _, err = c.conn.Write(buf[:n]); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *telnetClient) Receive() error {
+	buf := make([]byte, 1024)
+
+	n, err := c.conn.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	if _, err = c.out.Write(buf[:n]); err != nil {
+		return err
+	}
+
+	return nil
+}
