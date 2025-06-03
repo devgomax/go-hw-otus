d07f07176bcd1d2ea884f3af6d427883187e8ccb
Golubev Maksim Vladislavovich <maksgolubev@ozon.ru>
2025-06-03 22:00:35 +0600

    HW13 is completed

--- hw12_13_14_15_16_calendar/internal/storage/memory/storage_test.go
+++ hw12_13_14_15_16_calendar/internal/storage/memory/storage_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"slices"
 	"strconv"
+	"sync"
 	"testing"
 	"time"
 
@@ -90,4 +91,133 @@ func TestStorage(t *testing.T) {
 		require.Empty(t, repo.sortedEvents)
 		require.Empty(t, repo.eventsByUser[events[0].UserID])
 	})
+
+	t.Run("update event", func(t *testing.T) {
+		t.Cleanup(func() {
+			cleanup(repo)
+		})
+
+		err := repo.CreateEvent(context.Background(), events[0])
+		require.NoError(t, err)
+
+		eventUpd := &storage.Event{
+			ID:             events[0].ID,
+			Title:          "upd",
+			StartsAt:       ptr(time.Now()),
+			EndsAt:         ptr(time.Now()),
+			Description:    "upd",
+			UserID:         events[0].UserID,
+			NotifyInterval: 10 * time.Second,
+		}
+
+		err = repo.UpdateEvent(context.Background(), eventUpd)
+		require.NoError(t, err)
+
+		require.Equal(t, eventUpd, repo.eventsByID[events[0].ID])
+		require.Equal(t, eventUpd, repo.eventsByUser[events[0].UserID][0])
+		require.Equal(t, eventUpd, repo.sortedEvents[0])
+	})
+
+	t.Run("read events", func(t *testing.T) {
+		t.Cleanup(func() {
+			cleanup(repo)
+		})
+
+		userID := "user"
+		events := []*storage.Event{
+			{
+				Title:          "Today",
+				StartsAt:       ptr(start),
+				EndsAt:         ptr(start.Add(24 * time.Hour)),
+				Description:    "Description",
+				UserID:         userID,
+				NotifyInterval: 15 * time.Second,
+			},
+			{
+				Title:          "Title2",
+				StartsAt:       ptr(start.Add(2 * 24 * time.Hour)),
+				EndsAt:         ptr(start.Add(3 * 24 * time.Hour)),
+				Description:    "Description",
+				UserID:         userID,
+				NotifyInterval: 15 * time.Second,
+			},
+			{
+				Title:          "Title3",
+				StartsAt:       ptr(start.Add(14 * 24 * time.Hour)),
+				EndsAt:         ptr(start.Add(15 * 24 * time.Hour)),
+				Description:    "Description",
+				UserID:         userID,
+				NotifyInterval: 15 * time.Second,
+			},
+		}
+
+		for _, event := range events {
+			err := repo.CreateEvent(context.Background(), event)
+			require.NoError(t, err)
+		}
+
+		dailyEvents, err := repo.ReadDailyEvents(context.Background(), userID, start)
+		require.NoError(t, err)
+		require.Len(t, dailyEvents, 1)
+		require.Equal(t, events[0], dailyEvents[0])
+
+		dailyEvents, err = repo.ReadWeeklyEvents(context.Background(), userID, start)
+		require.NoError(t, err)
+		require.Equal(t, events[:2], dailyEvents)
+
+		dailyEvents, err = repo.ReadMonthlyEvents(context.Background(), userID, start)
+		require.NoError(t, err)
+		require.Equal(t, events, dailyEvents)
+	})
+}
+
+func TestStorageMultithreading(_ *testing.T) {
+	repo := New()
+	ctx := context.Background()
+
+	userID := "user"
+
+	start := time.Now()
+	end := start.Add(24 * time.Hour)
+
+	createFunc := func() {
+		for range 1000 {
+			_ = repo.CreateEvent(ctx, &storage.Event{
+				StartsAt: &start,
+				EndsAt:   &end,
+				UserID:   userID,
+			})
+		}
+	}
+
+	readFunc := func() {
+		for range 1000 {
+			_, _ = repo.ReadDailyEvents(ctx, userID, start)
+		}
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(4)
+
+	go func() {
+		defer wg.Done()
+		createFunc()
+	}()
+
+	go func() {
+		defer wg.Done()
+		createFunc()
+	}()
+
+	go func() {
+		defer wg.Done()
+		readFunc()
+	}()
+
+	go func() {
+		defer wg.Done()
+		readFunc()
+	}()
+
+	wg.Wait()
 }
