e58dbe8dd3f1ec12648e8781ead3549c1a3a3f67
Golubev Maksim Vladislavovich <maksgolubev@ozon.ru>
2025-06-07 19:42:29 +0600

    HW13 is completed

--- hw12_13_14_15_16_calendar/internal/app/interfaces.go
+++ hw12_13_14_15_16_calendar/internal/app/interfaces.go
@@ -7,12 +7,12 @@ import (
 	"github.com/devgomax/go-hw-otus/hw12_13_14_15_calendar/internal/storage"
 )
 
-// IApp основной API интерфейс
+// IApp основной API интерфейс.
 type IApp interface {
-	CreateEvent(ctx context.Context, title, description, userID string, startsAt, endAt *time.Time, notifyInterval time.Duration) error
+	CreateEvent(ctx context.Context, title, description, userID string, startsAt, endAt *time.Time, notifyInterval time.Duration) error //nolint:lll
 	DeleteEvent(ctx context.Context, eventID string) error
 	ReadDailyEvents(ctx context.Context, userID string, date time.Time) ([]*storage.Event, error)
 	ReadWeeklyEvents(ctx context.Context, userID string, date time.Time) ([]*storage.Event, error)
 	ReadMonthlyEvents(ctx context.Context, userID string, date time.Time) ([]*storage.Event, error)
-	UpdateEvent(ctx context.Context, id, title, description, userID string, startsAt, endAt *time.Time, notifyInterval time.Duration) error
+	UpdateEvent(ctx context.Context, id, title, description, userID string, startsAt, endAt *time.Time, notifyInterval time.Duration) error //nolint:lll
 }
